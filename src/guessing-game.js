0098ae2c8e26b4048df0600a6c23553ed24fabb6
Ilya Androsov <ilya_androsov@icloud.com>
2019-10-09 15:01:33 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.high = max;
+        this.low = min;
     }
 
     guess() {
-
+        return Math.round((this.low + this.high) / 2);
     }
 
     lower() {
-
+        this.high = this.guess();
     }
 
     greater() {
-
+        this.low = this.guess();
     }
 }
 
